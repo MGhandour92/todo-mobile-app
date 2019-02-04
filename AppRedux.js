1a042bf8f57aaf1dcb05c5790f581ff07b3c610f
Mohamed El Ghandour <m.el_ghandour@hotmail.com>
2019-02-04 09:59:20 +0200

    Storage (State/Redux)

---
+++ AppRedux.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { View } from 'react-native';
+import Title from './Components/Title';
+import Input from './Components/Input';
+import List from './Components/List';
+
+import { actionCreators } from './Components/reduxStorage'
+
+export default class AppRedux extends Component {
+
+  state = {}
+
+  componentWillMount() {
+    const {store} = this.props
+
+    const {todos} = store.getState()
+    this.setState({todos})
+
+    this.unsubscribe = store.subscribe(() => {
+      const {todos} = store.getState()
+      this.setState({todos})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  onAddTodo = (text) => {
+    const {store} = this.props
+
+    store.dispatch(actionCreators.add(text))
+  }
+
+  onRemoveTodo = (index) => {
+    const {store} = this.props
+
+    store.dispatch(actionCreators.remove(index))
+  }
+
+  render() {
+    const {todos} = this.state
+
+    return (
+      <View>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </View>
+    )
+  }
+}
---
+++ AppState.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { View } from 'react-native';
+import Title from './Components/Title';
+import Input from './Components/Input';
+import List from './Components/List';
+
+export default class AppState extends Component {
+
+  state = { todos: ['Todo 1', 'Todo 2', 'Todo 3', 'Todo 4'] }
+
+  //Adding todo item
+  onAddTodo = (text) => {
+    const { todos } = this.state // get the old todos
+    const newState = { todos: [text, ...todos] } // add new todo then add the old array of todos
+
+    this.setState(newState) //set with the new state
+  }
+
+
+  //remove todo by index
+  onRemoveTodo = (index) => {
+    const { todos } = this.state // get the old todos
+    const newState = { todos: todos.filter((todo, i) => i !== index) }
+    // ^ filter the todos to get all elements except the specified index
+    // [callback function] explanation:
+    // function filterTodo(todo, i)
+    // {
+    //   return i !== index;
+    // }
+
+    this.setState(newState) //set with the new state
+  }
+
+  render() {
+    const { todos } = this.state //get current todos array
+
+    return (
+      <View>
+        <Title>To-Do List</Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </View>
+    );
+  }
+}
